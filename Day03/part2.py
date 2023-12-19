03d5a5f9f67d76910d293290ac827e51a8770f8e
Russell Davidson <rbdchemistry@gmail.com>
2023-12-19 12:11:29 -0500

    removing copyrighted text... oops

--- Day03/part2.py
+++ Day03/part2.py
@@ -1,29 +1,6 @@
 ### QUESTION
 """
-... A gear is any * symbol that is adjacent to exactly two part numbers. Its 
-gear ratio is the result of multiplying those two numbers together.
-
-This time, you need to find the gear ratio of every gear and add them all up so 
-that the engineer can figure out which gear needs to be replaced.
-
-Consider the same engine schematic again:
-
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-
-In this schematic, there are two gears. The first is in the top left; it has 
-part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the 
-lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear 
-because it is only adjacent to one part number.) Adding up all of the gear 
-ratios produces 467835.
+https://adventofcode.com/2023/day/3
 """
 
 ### PREAMBLE
