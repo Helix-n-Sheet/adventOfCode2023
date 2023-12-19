f570f2ed8053ae4345cc23a0ea3fbe6a9931ef25
Russell Davidson <rbdchemistry@gmail.com>
2023-12-19 13:45:15 -0500

    pushing working part2 with forward and backward implementations; backwards is much more efficient.

--- Day05/part2.py
+++ Day05/part2.py
@@ -1,9 +1,31 @@
 ### QUESTION
 """
 https://adventofcode.com/2023/day/5
+
+NOTE: the smallest location is much smaller than the smallest seed number. So, 
+sort location numbers and then back calculate seed numbers, checking to see if
+each seed number is represented in the original list of seed ranges. Break once
+a seed ID is found. 
+This strategy has the potential to be slower since there are more location 
+numbers (4.1 billion) than seed numbers (1.7 billion) but the objective is find 
+the smallest location value so we should design the code with that in mind.
+
+Timing-wise, the forward process is slightly faster per iteration but will only
+exit once all seed numbers have been tested. The backward process will calculate
+fictional seeds but should exit as soon as a match with reality is found. 
+
+The backward implementation took a bit longer than I expected, but only took 
+
+real	1m48.883s
+user	1m48.875s
+sys	0m0.008s
+
+in the end! Meanwhile, the forward approach is still running and has been for
+hours.
 """
 
 ### PREAMBLE
+import timeit
 
 
 ### FUNCTIONS
@@ -44,9 +66,23 @@ def parse_almanac(almanac_string):
             dest_start, source_start, range_len = line.split()
             almanac_dict[dict_key].append((int(source_start),int(dest_start),int(range_len)))
     
-    ###
-    nSeeds = sum([seed_range[1] for seed_range in seed_ranges])
-    print(nSeeds)
+    #### INSPECTING SEEDS
+    #nSeeds = sum([seed_range[1] for seed_range in seed_ranges])
+    #print(nSeeds) --> 1,679,109,896
+    #print('Smallest seed value:', min([seed_range[0] for seed_range in seed_ranges]))
+    # --> Smallest seed value: 40,283,135
+    #print('Largest seed value:', max([seed_range[0]+seed_range[1] for seed_range in seed_ranges]))
+    # --> Largest seed value: 4,068,452,766
+
+    #### INSPECTING LOCATIONS 
+    #nLocations = sum([mapping_range[2] for mapping_range in almanac_dict['humidity-to-location']])
+    #nLocation_ranges = len([mapping_range[2] for mapping_range in almanac_dict['humidity-to-location']])
+    #print(nLocations) --> 4,122,156,611
+    #print(nLocation_ranges) --> 43
+    #print('Smallest location value:', min([mapping_range[1] for mapping_range in almanac_dict['humidity-to-location']]))
+    # --> Smallest location value: 0
+    #print('Largest location value:', max([mapping_range[1]+mapping_range[2] for mapping_range in almanac_dict['humidity-to-location']]))
+    # --> Largest location value: 4,294,967,296
 
     return almanac_dict
 
@@ -58,10 +94,13 @@ def parse_seed_block(seed_line):
     """
     numbers = [int(number) for number in seed_line.split(':')[1].split()]
     number_pairs = list(zip(numbers[::2],numbers[1::2]))
-    print('Number of seed ranges:', len(number_pairs))
+    #print('Number of seed ranges:', len(number_pairs))
     return number_pairs
 
 
+# ------------------------------------------------------------------------------
+# BRUTE FORCE - FORWARD APPROACH
+# ------------------------------------------------------------------------------
 def determine_next_attribute(source_index,ranges_list):
     """
     check the mapping_dict to see if index is within source ranges explicitly 
@@ -100,12 +139,14 @@ def get_seed_location(seed_number, almanac_dict):
     return location_number
 
 
-def get_smallest_location(almanac_string):
+def forward_get_smallest_location(almanac_string):
     """
     run the analysis
     """
+    # gather the mapping and seed ranges
     almanac_dict = parse_almanac(almanac_string)
-    min_location = 99999999
+    #
+    min_location = float('inf')
     for seed_range in almanac_dict['seeds']:
         _range = range(seed_range[0],seed_range[0]+seed_range[1])
         for seed in _range:
@@ -113,6 +154,67 @@ def get_smallest_location(almanac_string):
             if loc < min_location:
                 min_location = loc
     return min_location
+
+
+# ------------------------------------------------------------------------------
+# BACKWARD APPROACH
+# ------------------------------------------------------------------------------
+def determine_previous_attribute(destination_index, ranges_list):
+    """
+    check the mapping_dict to see if index is within destination ranges 
+    explicitly  defined. If it does sit in a defined range, return the 
+    associated source value. Else, the mapping is 1:1 for source and 
+    destination. 
+    """
+    # loop over ranges defined in ranges_list
+    for _range in ranges_list:
+        # boolean test to see the index is in the destination range defined by 
+        # _range[1] <= index < _range[1]+_range[2]
+        if destination_index >= _range[1] and destination_index < _range[1]+_range[2]:
+            # if true, then the source index is equal to the source_start value 
+            # plus the difference between index and destination_start values. 
+            return _range[0]+destination_index-_range[1]
+    
+    # if the destination_index value is not within any of the explicitly defined
+    # ranges, then return the same index.
+    return destination_index
+
+
+def get_location_seed(loc_number, almanac_dict):
+    """
+    jump from 
+    location->humidity->temperature->light->water->fertilizer->soil->seed to 
+    get the location's seed number
+    """
+    humidity_number = determine_previous_attribute(loc_number,almanac_dict['humidity-to-location'])
+    temperature_number = determine_previous_attribute(humidity_number,almanac_dict['temperature-to-humidity'])
+    light_number = determine_previous_attribute(temperature_number,almanac_dict['light-to-temperature'])
+    water_number = determine_previous_attribute(light_number,almanac_dict['water-to-light'])
+    fertilizer_number = determine_previous_attribute(water_number,almanac_dict['fertilizer-to-water'])
+    soil_number = determine_previous_attribute(fertilizer_number,almanac_dict['soil-to-fertilizer'])
+    seed_number = determine_previous_attribute(soil_number,almanac_dict['seed-to-soil'])
+    #print(seed_number, soil_number, fertilizer_number, water_number, light_number, temperature_number, humidity_number, loc_number)
+    return seed_number
+
+
+def backward_get_smallest_location(almanac_string):
+    """
+    run the analysis
+    """
+    # gather the mapping and seed ranges
+    almanac_dict = parse_almanac(almanac_string)
+    # determine the seed ranges before hand
+    seed_ranges = [(start,start+range_len) for start, range_len in almanac_dict['seeds']]
+    
+    # loop over possible location numbers until one matches with a seed number
+    loc = 0
+    while True:
+        seed = get_location_seed(loc, almanac_dict)
+        #print(seed,loc)
+        if any([start <= seed < end for start, end in seed_ranges]):
+            return loc
+        loc += 1
+
 
 ### TEST HANDLING
 if __name__ == '__main__':
@@ -150,13 +252,21 @@ humidity-to-location map:
 60 56 37
 56 93 4"""
 
-    value = get_smallest_location(almanac_string)
+    value = forward_get_smallest_location(almanac_string)
     assert value == 46
+    t = timeit.Timer(lambda: forward_get_smallest_location(almanac_string))
+    print('Forward implementation:', t.timeit(100), '<usec>')
+    print('\n\n')
+
+    value = backward_get_smallest_location(almanac_string)
+    assert value == 46
+    t = timeit.Timer(lambda: backward_get_smallest_location(almanac_string))
+    print('Backward implementation:', t.timeit(100), '<usec>')
     print('\n\n')
 
     with open('./almanac.txt','r') as infile:
         almanac_string = infile.read()
-    value = get_smallest_location(almanac_string)
+    value = backward_get_smallest_location(almanac_string)
     print(value)
 
 
